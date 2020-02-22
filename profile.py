cf3e94ce35b3b6e14e0adf9e908ce880d8740590
Mitchell Greer <mitchsgreer@gmail.com>
2020-02-22 12:02:28 -0800

    Added str to User class.

--- profile.py
+++ profile.py
@@ -2,7 +2,7 @@
 
 class User:
 
-    def __init__(userID=-1, name="", gender=None, desired_gender=None, picture="", yup=[], nup=[]):
+    def __init__(self, userID=-1, name="No Name", gender=None, desired_gender=None, picture={"name": "", "picture": ""}, yup=[], nup=[]):
         self.userID = userID
         self.name = name
         self.gender = gender
@@ -12,7 +12,35 @@ class User:
         self.nup = nup
 
     def __str__(self):
-        pass
+
+        out_string = (
+            str(self.name)
+            + ":\n\tUser ID: "
+            + str(self.userID)
+            + "\n\tGender: "
+            + str(self.gender)
+            + "\n\tDesired Gender: "
+            + str(self.desired_gender)
+            + "\n\tPicture: "
+            + str(self.picture["name"])
+            + "\n\tYup List: "
+        )
+
+        for item in self.yup:
+            out_string += "\t"
+            out_string += item["name"] + ", "
+
+        out_string += "\n\tNup List: "
+
+        for item in self.nup:
+            out_string += "\t"
+            out_string += item["name"] + ", "
+
+        return out_string
 
     def __rpr__(self):
         pass
+
+
+new_user = User()
+print(new_user)
