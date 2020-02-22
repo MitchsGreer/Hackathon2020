18c49ec3e9f85a31af0fd87d712402af37d97690
Mitchell Greer <mitchsgreer@gmail.com>
2020-02-22 14:02:40 -0800

    changed the layout of the files, and travis file.

--- src/face_recognition.py
+++ src/face_recognition.py
@@ -27,7 +27,7 @@ def collate_fn(x):
 
 
 print(os.listdir("data/test_images"))
-dataset = datasets.ImageFolder("data/test_images")
+dataset = datasets.ImageFolder("data/")
 dataset.idx_to_class = {i: c for c, i in dataset.class_to_idx.items()}
 loader = DataLoader(dataset, collate_fn=collate_fn, num_workers=workers)
 
